55f897ec16a4d16dcf4946e094a3e6e975bd27c8
muchpretty99 <muchpretty99@ibizsoftware.net>
2023-08-02 16:20:25 +0900

    context 사용하기

---
+++ src/components/Options.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Options = ({ name, updateItemCount }) => {
+  return (
+    <form>
+      <input
+        type="checkbox"
+        id={`${name} options`}
+        onChange={(e) => updateItemCount(name, e.target.checked ? 1 : 0)}
+      />
+      <label htmlFor={`${name} options`}>{name}</label>
+    </form>
+  );
+};
+
+export default Options;
